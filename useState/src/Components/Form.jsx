8c5d61bdb7c13bb349824b5cb26364e66cab7aeb
anusha-c18 <anushac2001@gmail.com>
2024-01-28 16:12:04 +0530

    restructure

---
+++ useState/src/Components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div className="form">
+      <input type="text" name="task-text" id="task-text" />
+      <button className="add">Add</button>
+    </div>
+  );
+}
+
+export default Form;
